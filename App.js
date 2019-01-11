d9c224f8dbf0b89b61988467458c059df13353ff
justin <jrohweller13@apu.edu>
2019-01-10 22:48:54 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { AppLoading, Asset, Font } from 'expo';
+// import DonkeyKong from "react-native-donkey-kong";
+import DonkeyKong from './react-native-donkey-kong';
+import Preload from "./Preload";
+
+export default class App extends React.Component {
+  state = {
+    ready: false
+  }
+
+  async _loadAssetsAsync() {
+    await Promise.all([
+      ...Preload.images.map(x => Asset.fromModule(x).downloadAsync()),
+      ...Preload.fonts.map(x => Font.loadAsync(x))
+    ])
+  }
+
+  render() {
+    // if (!this.state.ready) {
+    //   return (
+    //     <AppLoading
+    //       startAsync={this._loadAssetsAsync}
+    //       onFinish={() => this.setState({ ready: true })}
+    //     />
+    //   );
+    // }
+
+    return (
+      <DonkeyKong
+        // theme={{
+        //   $donkeyKongMenuFont: "ArcadeClassic"
+        // }}
+      />
+    );
+  }
+}
+
+//app.json
+// "icon": "./assets/icon.png",
+    // "splash": {
+    //   "image": "./assets/splash.png",
+    //   "resizeMode": "contain",
+    //   "backgroundColor": "#ffffff"
+    // },

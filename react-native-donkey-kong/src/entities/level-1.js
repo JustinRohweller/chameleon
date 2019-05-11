31c903c84cb7929d55bfa05727a1d3e292a4312f
Unknown <jrohweller13@apu.edu>
2019-05-11 15:46:17 -0700

    Deleting more things

--- react-native-donkey-kong/src/entities/level-1.js
+++ react-native-donkey-kong/src/entities/level-1.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Dimensions } from "react-native";
 import Matter from "matter-js";
-import Platform from "../components/platform";
 import Barrier from "../components/barrier";
 import Ladder from "../components/ladder";
 import Mario from "../components/mario";
@@ -38,145 +37,6 @@ export default restart => {
             false,
             "left"
         ),
-        // ladder2: Ladder(
-        //     world,
-        //     { x: cx + 100 * scale, y: offsetY + 130 },
-        //     60,
-        //     true,
-        //     true,
-        //     "left"
-        // ),
-        // ladder3: Ladder(
-        //     world,
-        //     { x: cx - 70, y: offsetY + 200 },
-        //     60,
-        //     true,
-        //     true,
-        //     "right"
-        // ),
-        // ladder4: Ladder(world, { x: cx + 70, y: offsetY + 178 }, 36),
-        // ladder5: Ladder(world, { x: cx + 70, y: offsetY + 230 }, 36),
-        // ladder6: Ladder(
-        //     world,
-        //     { x: cx + 105 * scale, y: offsetY + 275 },
-        //     50,
-        //     true,
-        //     true,
-        //     "left"
-        // ),
-        // ladder7: Ladder(world, { x: cx - 100 * scale, y: offsetY + 243 }, 36),
-        // ladder8: Ladder(world, { x: cx - 100 * scale, y: offsetY + 295 }, 36),
-        // ladder9: Ladder(
-        //     world,
-        //     { x: cx - 100 * scale, y: offsetY + 354 },
-        //     60,
-        //     true,
-        //     true,
-        //     "right"
-        // ),
-        // ladder10: Ladder(
-        //     world,
-        //     { x: cx, y: offsetY + 352 },
-        //     66,
-        //     true,
-        //     true,
-        //     "right"
-        // ),
-        // ladder11: Ladder(
-        //     world,
-        //     { x: cx + 90 * scale, y: offsetY + 430 },
-        //     70,
-        //     true,
-        //     false,
-        //     "left"
-        // ),
-        // ladder12: Ladder(
-        //     world,
-        //     { x: cx + 90 * scale, y: offsetY + 480 },
-        //     70,
-        //     true,
-        //     false,
-        //     "left"
-        // ),
-
-        // platform1: Platform(
-        //     world,
-        //     { x: cx, y: offsetY + 35 },
-        //     0,
-        //     platformWidth * 0.25
-        // ),
-        // platform2: Platform(
-        //     world,
-        //     { x: cx - 15, y: offsetY + 100 },
-        //     0,
-        //     platformWidth * 0.8
-        // ),
-        // platform3: Platform(
-        //     world,
-        //     { x: cx + 15, y: offsetY + 165 },
-        //     -0.07,
-        //     platformWidth * 0.8
-        // ),
-        // platform4: Platform(
-        //     world,
-        //     { x: cx - 15, y: offsetY + 240 },
-        //     0.07,
-        //     platformWidth * 0.8
-        // ),
-        // platform5: Platform(
-        //     world,
-        //     { x: cx + 15, y: offsetY + 315 },
-        //     -0.07,
-        //     platformWidth * 0.8
-        // ),
-        // platform6: Platform(
-        //     world,
-        //     { x: cx - 15, y: offsetY + 390 },
-        //     0.07,
-        //     platformWidth * 0.8
-        // ),
-        // platform7: Platform(
-        //     world,
-        //     { x: cx, y: offsetY + 465 },
-        //     0,
-        //     platformWidth * 0.9
-        // ),
-
-        // barrier1: Barrier(
-        //     world,
-        //     { x: cx - platformWidth / 2 + 10, y: cy },
-        //     height
-        // ),
-        // barrier2: Barrier(
-        //     world,
-        //     {
-        //         x: cx + platformWidth / 2 - 10,
-        //         y: cy - offsetY - 200
-        //     },
-        //     height
-        // ),
-        // barrier3: Barrier(
-        //     world,
-        //     {
-        //         x: cx + platformWidth / 2 - 10,
-        //         y: Math.min(1000, cy - offsetY - 200 + height) // Matter will ignore objects that start outside of a 1000x1000 box
-        //     },
-        //     height,
-        //     collisionCategories.mario
-        // ),
-        // oil: {
-        //     source: require("../components/props/oil.gif"),
-        //     position: { x: cx - 140 * scale, y: offsetY + 427 },
-        //     size: { width: 30, height: 56 },
-        //     renderer: <Tile />
-        // },
-
-        // barrelStack: {
-        //     source: require("../components/props/barrel-stack.gif"),
-        //     position: { x: cx - platformWidth * 0.125 + 22, y: offsetY + 67 },
-        //     size: { width: 55, height: 44 },
-        //     renderer: <Tile />
-        // },
 
         // Princess is referenced in the camera.js. She is the goal. Replace with goal as name.
         princess: {
@@ -185,11 +45,6 @@ export default restart => {
             size: { width: 75, height: 45 },
             renderer: <Tile />
         },
-
-        // kong: Kong(world, {
-        //     x: cx - platformWidth * 0.4 + 43,
-        //     y: offsetY + 55
-        // }),
 
         mario: Mario(world, { x: cx, y: offsetY + 465 - 20 / 2 - 20 }),
 

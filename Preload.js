cdcf2f061f768ac2c912c848a00b94f245ab8861
Unknown <jrohweller13@apu.edu>
2019-05-11 15:47:57 -0700

    Removed more unused

--- Preload.js
+++ Preload.js
@@ -16,19 +16,6 @@ export default {
     require("./node_modules/react-native-donkey-kong/src/components/mario/mario-rising.gif"),
     require("./node_modules/react-native-donkey-kong/src/components/mario/mario-walking-hammering.gif"),
     require("./node_modules/react-native-donkey-kong/src/components/mario/mario-walking.gif"),
-    // require("./node_modules/react-native-donkey-kong/src/components/props/barrel-stack.gif"),
-    // require("./node_modules/react-native-donkey-kong/src/components/props/explosion-radial.gif"),
-    // require("./node_modules/react-native-donkey-kong/src/components/props/hammer.gif"),
-    // require("./node_modules/react-native-donkey-kong/src/components/props/oil.gif"),
-    // require("./node_modules/react-native-donkey-kong/src/components/props/princess.gif"),
-    // require("./node_modules/react-native-donkey-kong/src/components/props/sparks.gif"),
-    // require("./node_modules/react-native-donkey-kong/src/menus/game-over/wise-monkey.gif"),
-    // require("./node_modules/react-native-donkey-kong/src/menus/princess-rescued/love-heart.gif"),
-    // require("./node_modules/react-native-donkey-kong/src/menus/princess-rescued/mario-large.gif"),
-    // require("./node_modules/react-native-donkey-kong/src/menus/princess-rescued/princess-large.gif")
-    //-- Preloading these .png causes them to be displayed overly-large in the game...
-    //require("./node_modules/react-native-donkey-kong/src/components/ladder/ladder.png"),
-    //require("./node_modules/react-native-donkey-kong/src/components/platform/platform.png")
   ],
   // fonts: [
   //   { ArcadeClassic: require("./assets/fonts/ArcadeClassic.ttf") },
